5e2c84ac42e84a410b01ab6e7a8019f56207b252
Julian <julianarruti006@gmail.com>
2024-04-28 02:23:37 -0300

    Update main.py
    Se instalo las librerias faltantes: chromadb, uuid, langchain.
    
    Se incializo el cliente chromadb y se configuro la coleccion.
    
    Se ejecuto la funcion de procesamiento de chunks del documento y se agrego a la chromadb

--- Bot-RGA/main.py
+++ Bot-RGA/main.py
@@ -1,17 +1,53 @@
 from fastapi import FastAPI, Request
 import cohere
 import os
+import chromadb
+import uuid
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 app = FastAPI()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DOC_PATH = os.path.join(BASE_DIR, 'data', 'documento.txt')
-
 with open (DOC_PATH, 'r', encoding="utf-8") as file:
     document_content = file.read()
 
-print(document_content)
+# Initialize ChromaDB client and collection
+chroma_client = chromadb.Client()
+collection = chroma_client.get_or_create_collection(
+    name="my_collection", metadata={"hnsw:space": "ip"}  # Adjust space if needed
+)
+
+co = cohere.Client("G6m6NXL8hYwNWUiXvtMYxfch6BgQk2RpXvg4uTXS")
+
+# Function to split document content into chunks with embeddings
+def process_document(document_path):
+    # Read document content
+    with open(document_path, "r", encoding="utf-8") as file: ###
+        document_content = file.read()  ###
+
+    # Split content using RecursiveCharacterTextSplitter
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=["."], chunk_size=50, chunk_overlap=20
+    )
+    docs = text_splitter.create_documents([document_content])
+
+    # Generate embeddings and add them to the collection
+    document_ids = []
+    for doc in docs:
+        uuid_name = str(uuid.uuid1())
+        print(f"Adding embedding with ID: {uuid_name}")
+        embedding_data = co.embed(
+            texts=[doc.page_content], model="embed-multilingual-v3.0", input_type="classification"
+        ).embeddings
+        collection.add(documents=[doc.page_content], ids=[uuid_name], embeddings=embedding_data)
+        document_ids.append(uuid_name)
+
+    return document_ids  # Return IDs for future doc reference
+
+#Testing process_document(document_path)
+print(process_document(DOC_PATH))
 
 @app.get("/")
 async def root():
@@ -21,5 +57,3 @@ async def root():
     except Exception as e:
         print(f"Error initializing Cohere client: {e}")
     return {"message": "Hello, world!"}
-
-
